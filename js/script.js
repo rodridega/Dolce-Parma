3dec60df87bdef84ef19964edd85e0a63da5721f
Rodrigo Deganutti <rodridega@gmail.com>
2022-01-21 21:12:32 -0300

    Arreglando carrito

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 class Producto {
-  constructor(nombre, imagen, precio, stock) {
+  constructor(nombre, imagen, precio, stock, id) {
     this.nombre = nombre;
     this.imagen = imagen;
     this.precio = precio;
     this.stock = stock;
+    this.id = id;
   }
   mostrarInfo() {
     alert(`Elegiste ${this.nombre} a un valor de $${this.precio}`);
@@ -14,15 +15,16 @@ class Producto {
 
 const productos = [];
 productos.push(
-  new Producto("Cartuchera", "imagenes/producto (1).jpeg", 450, 3),
-  new Producto("Bolso", "imagenes/producto (2).jpeg", 1500, 2),
-  new Producto("Portacosmeticos", "imagenes/producto (3).jpeg", 720, 4)
+  new Producto("Cartuchera", "imagenes/producto (1).jpeg", 450, 3, 1),
+  new Producto("Bolso", "imagenes/producto (2).jpeg", 1500, 2, 2),
+  new Producto("Portacosmeticos", "imagenes/producto (3).jpeg", 720, 4, 3),
+  new Producto("Portacosmeticos", "imagenes/producto (4).jpeg", 300, 1, 4),
+  new Producto("Cartuchera", "imagenes/producto (5).jpeg", 400, 2, 5),
+  new Producto("Portacosmeticos", "imagenes/producto (6).jpeg", 1000, 5, 6)
 );
-const prodElegidos = [];
 
 
 const carrito = document.querySelector('#carrito')
-const contenedorCarrito = document.querySelector('#listaCarrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciarCarrito');
 const divCard = document.getElementById("cards");
 let productosCarrito = []
@@ -31,105 +33,88 @@ let productosCarrito = []
 
 /*FUNCIONES */
 
-
-
 // DIBUJA LAS CARDS
-
-function dibujarCard(producto) {
+productos.forEach((producto) =>{
   const colCard = document.createElement("div");
   colCard.className = "col";
-  colCard.setAttribute("data-bs-target", "#exampleModal");
-  colCard.setAttribute("data-bs-toggle", "modal");
-  colCard.addEventListener("click", mostrarModal);
+  colCard.addEventListener("click", () => mostrarModal(producto));
   divCard.appendChild(colCard);
-  const divCard2 = document.createElement("div");
-  divCard2.className = "card";
-  colCard.appendChild(divCard2);
-  const cardImg = document.createElement("img");
-  cardImg.setAttribute("src", producto.imagen);
-  cardImg.setAttribute("alt", "producto");
-  divCard2.appendChild(cardImg);
-  const bodyDiv = document.createElement("div");
-  bodyDiv.className = "card-body";
-  divCard2.appendChild(bodyDiv);
-  const cardTitle = document.createElement("h5");
-  cardTitle.innerText = producto.nombre;
-  bodyDiv.appendChild(cardTitle);
-  const stock = document.createElement("p");
-  stock.innerText = `Stock: ${producto.stock}`;
-  stock.setAttribute("id", "stock");
-  bodyDiv.appendChild(stock);
-  const cardPrecio = document.createElement("p");
-  cardPrecio.innerText = `$ ${producto.precio}`;
-  cardPrecio.setAttribute("id", "precio");
-  bodyDiv.appendChild(cardPrecio);
-  const btnCompra = document.createElement("button");
-  btnCompra.innerText = "Agregar al Carrito";
-  btnCompra.className = "btn btn-primary col-12 agregarCarrito";
-  btnCompra.addEventListener('click', agregarProducto)
-  bodyDiv.appendChild(btnCompra);
-}
+  colCard.innerHTML = 
+  `<div class="card" id="prod-${producto.id}" >
+      <img src="${producto.imagen} " class="card-img-top" alt="producto" data-bs-target="#exampleModal" data-bs-toggle="modal">
+      <div class="card-body">
+        <h5 class="card-title">${producto.nombre} </h5>
+        <p class="card-text" id="stock"> Stock: ${producto.stock} </p>
+        <p class="card-text" id="precio">$${producto.precio} </p>
+        <button class="btn btn-primary col-12 agregarCarrito">Agregar al Carrito</button>
+      </div>
+    </div>`
+    const btnCompra = document.querySelector(`#prod-${producto.id} .agregarCarrito`)
+    btnCompra.addEventListener('click',  () => agregarProducto(producto))
+    
+})
 
-for (let i = 0; i < productos.length; i++) {
-  dibujarCard(productos[i]);
-}
 
 //MUESTRA UN MODAL DE LA CARD ELEGIDA
 
-function mostrarModal(e) {
-  const cardElejida = e.target.parentElement.parentElement;
+function mostrarModal(producto) {
   const modal = document.getElementById("myModal");
-  const compraModal = document.getElementById("agregarCarrito");
   
+  modal.firstElementChild.setAttribute("src",`${producto.imagen}`);
+  modal.children[1].children[0].innerHTML = `${producto.nombre} `;
+  modal.children[1].children[1].children[0].innerHTML = `${producto.precio} `;
+  modal.children[1].children[1].children[1].innerHTML = `${producto.stock} `;
+  const btnModal = document.querySelector('.modal-footer .agregarCarrito')
+  btnModal.addEventListener("click", () => agregarProducto(producto))
 
-
-  modal.firstElementChild.setAttribute("src", cardElejida.querySelector("img").src);
-  modal.children[1].children[0].innerHTML = cardElejida.querySelector("h5").textContent;
-  modal.children[1].children[1].children[0].innerHTML = cardElejida.querySelector("#precio").textContent;
-  modal.children[1].children[1].children[1].innerHTML = cardElejida.querySelector("#stock").textContent;
-
-  
-
-  
 }
 
+//AGREGA PRODUCTO AL CARRITO
 
-//Agrega producto
-function agregarProducto(e){
-    e.stopPropagation
-if (e.target.classList.contains('agregarCarrito')) {
-    const prodSeleccionado = e.target.parentElement.parentElement;
-  leerProd(prodSeleccionado)
-  }
+function agregarProducto(producto){
+  debugger
+  const btnCompra = document.querySelector(`#prod-${producto.id} .agregarCarrito`)
+  btnCompra.innerHTML = "Quitar del carrito"
+  btnCompra.classList ='btn btn-danger col-12'
+  productosCarrito.push(producto)
+  
+  renderCarrito();
 }
+
+function vaciarCarrito(){
+  productosCarrito = [];
+  renderCarrito();
+}
+vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 
 //LEE DATOS DEL PRODUCTO
-function leerProd(producto){
 
-  const infoCard = {
-    nombre: producto.querySelector("h5").textContent,
-    imagen: producto.querySelector("img").src,
-    stock: producto.querySelector("#stock").textContent,
-    precio: producto.querySelector("#precio").textContent,
-  }
-  productosCarrito = [infoCard,...productosCarrito ]
-  dibujarCarrito(infoCard)
+function renderCarrito(){
+  let total = 0;
+  const tBody = document.querySelector("#tbody")
+  tBody.innerHTML= ""
+  let htm = ""
+  productosCarrito.forEach((producto) => {
+    total += producto.precio
+    htm += 
+    ` <tr>
+        <th scope="row">
+          <img src="${producto.imagen}" width="100px">
+        </th>
+        <td>${producto.nombre} </td>
+        <td>${producto.precio} </td>
+      </tr>`
+  } )
+  htm += `<tr>
+  <th scope="row">
+    Total
+  </th>
+ 
+  <td colspan="2" class="text-end"> $ ${total}</td>
+</tr>`
+  tBody.innerHTML= htm
+  
+  
 }
 
-// DIBUJAR EN EL CARRITO
-function dibujarCarrito(prod){
-  const tRow = document.createElement('tr')
-  const tRowImg = document.createElement('th');
-  const tRowName = document.createElement('th');
-  const tRowPrecio = document.createElement('th');
-  const img = document.createElement('img')
-  contenedorCarrito.appendChild(tRow)
-  tRow.appendChild(tRowImg)
-  tRow.appendChild(tRowName)
-  tRow.appendChild(tRowPrecio)
-  tRowImg.appendChild(img);
-  img.setAttribute('src', prod.imagen)
-  img.setAttribute('width', '100px')
-  tRowName.innerHTML = prod.nombre
-  tRowPrecio.innerHTML = prod.precio
-}
+
